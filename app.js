ca5b986625d81512b19fd62b45ed6dc293f46f8a
VIVEK KUMAR VERMA <vverma508@gmail.com>
2017-09-19 00:16:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', function(req, res) {
-    res.send("testing");
+    res.send(index);
 })
 
 app.get("/read", db.read);
